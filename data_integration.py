b8480b2582f136c3d709be140d1436154889b7e0
Shirley Qian Jin <50817937+shirley-jin@users.noreply.github.com>
2021-04-22 12:58:02 +0800

    update

--- data_integration.py
+++ data_integration.py
@@ -1,16 +1,18 @@
 import pandas as pd
 
-file = pd.read_excel("/Users/qianjin/Dropbox/PhD Project/OpenRiceData/Network data/Network_April21/network_user.xlsx")
+file = pd.read_excel(PATH)
 
 user_id = []
 topic=[]
 gamma=[]
 l=0
+#n is the number of categories in your dataset
+n=21
 
 def unique_index(L,e):
     return [i for (i,j) in enumerate(L) if j == e][-1]
 
-for i in range(1,21):
+for i in range(1,n):
     for j in range(0,len(file[file["topic"]==i])):
         if file[file["topic"]==i].loc[j+l]["user_id"] in user_id:
             if topic[unique_index(user_id,file[file["topic"]==i].loc[j+l]["user_id"])] == i:
@@ -29,4 +31,4 @@ for i in range(1,21):
     
 cb = pd.DataFrame({"user_id":user_id,"topic":topic,"gamma":gamma})
 
-cb.to_csv("/users/qianjin/desktop/network_user.csv")
+cb.to_csv(NEW_PATH)
